d174cbf2174ff60ab50d50b578b08b3aacdff14d
itsjatinjoshi <jatinjoshi2283@gmail.com>
2019-09-01 06:24:09 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jetpackkotlin/view/ListFragment.kt
@@ -0,0 +1,27 @@
+package com.example.jetpackkotlin.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+import com.example.jetpackkotlin.R
+import kotlinx.android.synthetic.main.fragment_list.*
+
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+
+
+}
